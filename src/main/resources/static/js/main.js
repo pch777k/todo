5a67ca3d9f99b15d5e07517a7012e05aa92dc24d
Pawel Chmiel <pch777@gazeta.pl>
2022-02-07 09:07:05 +0100

    refactoring

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,30 +1,29 @@
 $(document).ready(function () {
 
-	$('.eBtn').on('click', function (event) {
-		event.preventDefault();
-        var href = $(this).attr('href');
-        
-        $.get(href, function (task, status) {
-        	$('.updateForm #id').val(task.id);
-            $('.updateForm #description').val(task.description);
-            $('.updateForm #deadline').val(task.deadline);
-            $('.updateForm #priority').val(task.priority);
-            $('.updateForm #completed').val();
-            if(task.completed) {          	   
-            	$(".updateForm #completed").prop('checked', true);
-            } else {
-            	$(".updateForm #completed").prop('checked', false);
-            }
-        });
-        $('.updateForm #updateModal').modal();
-
-	});
-
-  //for delete 
-    $('.delBtn').on('click', function(event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        $('#removeModal #delRef').attr('href', href);
-        $('#removeModal').modal();
-    });
+	    $('.eBtn').on('click', function (event) {
+	        event.preventDefault();
+	        var href = $(this).attr('href');
+	        
+	            $.get(href, function (task) {
+	                $('.updateForm #id').val(task.id);
+	                $('.updateForm #description').val(task.description);
+	                $('.updateForm #deadline').val(task.deadline);
+	                $('.updateForm #priority').val(task.priority);
+	                $('.updateForm #completed').val();
+	                if(task.completed) {          	   
+	                	$(".updateForm #completed").prop('checked', true);
+	                } else {
+	                	$(".updateForm #completed").prop('checked', false);
+	                }
+	            });
+	            $('.updateForm #updateModal').modal();
+	    });
+	    
+	    $('.delBtn').on('click', function(event) {
+	        event.preventDefault();
+	        var href = $(this).attr('href');
+	        $('#removeModal #delRef').attr('href', href);
+	        $('#removeModal').modal();
+	    });
+	    
 });
